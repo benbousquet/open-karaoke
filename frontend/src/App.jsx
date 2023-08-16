336ad8478b35c0a811e55d94292d79a2cee07c09
benbousquet <bbousque@asu.edu>
2023-08-16 00:40:08 -0700

    Revert "added basic lyric and player functionality"
    This reverts commit 6f0545658247b4b8fa0d07e277a5056cd903d5e9.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import Karaoke from "./components/Karaoke";
-
 function App() {
-  return <Karaoke />;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
