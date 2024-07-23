16ea6237c51fde51a5460de34b23d78ef6977bb1
benbousquet <bbousque@asu.edu>
2024-07-22 21:08:11 -0700

    wip:create timeline

---
+++ src/components/timeline.tsx
@@ -0,0 +1,10 @@
+export default function Timeline() {
+  // 11 lines
+  // 2-3 seconds per section
+
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+}
---
+++ src/utils/types.tsx
@@ -0,0 +1,15 @@
+enum Key {
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+}
+
+type Note = {
+  key: Key; // musical
+  start: Number; // milliseconds
+  duration: Number; // milliseconds
+};
